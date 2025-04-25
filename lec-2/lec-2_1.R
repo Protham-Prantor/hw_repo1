9636e137da3561fba0d5cb241710a7fd05bba404
Protham-Prantor <prothamprantor@gmail.com>
2025-04-26 01:07:53 +0600

    homeworks on lecture 2

---
+++ lec-2/lec-2_1.R
@@ -0,0 +1,4 @@
+x <- 2025 - 2023 #difference between current year and the year I started my uni life
+y <- 2025 - 2004 # difference between current year and my birth year
+z <- x / y #age divided by time spent at the university
+z <- z * 100 #percentage of life spent at the university
